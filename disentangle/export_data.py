b9c49fc94b63e0beab907e9c736c85573f9f4ac3
nmehlman <nmehlman@usc.edu>
2026-01-23 12:47:48 -0800

    Data export code

---
+++ disentangle/export_data.py
@@ -0,0 +1,86 @@
+from fileinput import filename
+from data.expresso import ExpressoDataset, EXPRESSO_SR
+from data.msp_podcast import MSPPodcastDataset, MSP_SR
+from network.codec import HifiCodec, EnCodec, BigCodec, HIFICODEC_SR, ENCODEC_SR, BIGCODEC_SR
+import tqdm
+import torch
+import argparse
+import pickle
+import os
+import yaml
+
+from network.models import VoxProfileEmotionModel
+
+CODECS = {
+    "encodec": (EnCodec, ENCODEC_SR),
+    "hificodec": (HifiCodec, HIFICODEC_SR),
+    "bigcodec": (BigCodec, BIGCODEC_SR),
+}
+
+DATASETS = {
+    "expresso": (ExpressoDataset, EXPRESSO_SR),
+    "msp_podcast": (MSPPodcastDataset, MSP_SR),
+}
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Export codec embeddings.")
+    
+    parser.add_argument(
+        "--config",
+        type=str,
+        required=True,
+        help="Path to the configuration file."
+    )
+    
+    args = parser.parse_args()
+
+    with open(args.config, "r") as f:
+        config = yaml.safe_load(f)
+    
+    print(f"Exporting:\n\tCodec: {config['codec']}\n\tDataset: {config['dataset']}\n\tDevice: {config['device']}")
+    
+    save_root = config["save_path"]
+    os.makedirs(save_root, exist_ok=True)
+
+    codec_class, codec_sr = CODECS[config["codec"]]
+    dataset_class, dataset_sr = DATASETS[config["dataset"]]
+
+    emotion_model = VoxProfileEmotionModel(device=config["device"], split_models=True)
+
+    codec = codec_class(device=config["device"])
+    dataset = dataset_class(**config["data_args"])
+
+    for sample in tqdm.tqdm(dataset, total=len(dataset), desc="Exporting Data"):
+        
+        audio = sample["audio"].to(config["device"])
+        label = sample["emotion"]
+        filename = sample["filename"]
+        length = sample["length"]
+
+        emotion_logits, emotion_embedding = emotion_model(
+            audio, sr=dataset_sr, return_embeddings=True, lengths=torch.tensor([length]).to(config["device"])
+        )
+        
+        with torch.no_grad():
+            embeddings = codec.encode(audio, sr=dataset_sr)
+            codes, quantized_embeddings = codec.quantize(embeddings)
+
+        codes = codes.squeeze()
+        quantized_embeddings = quantized_embeddings.squeeze()
+
+        save_dict = {
+                "filename": filename,
+                "label": label,
+                "codes": codes.cpu(),
+                "quantized_embedding": quantized_embeddings.cpu(),
+                "raw_embedding": embeddings.cpu().squeeze(),
+                "whisper_emotion_logits": emotion_logits["whisper_logits"].cpu().squeeze(),
+                "wavlm_emotion_logits": emotion_logits["wavlm_logits"].cpu().squeeze(),
+                "whisper_emotion_embedding": emotion_embedding["whisper_embedding"].cpu().squeeze(),
+                "wavlm_emotion_embedding": emotion_embedding["wavlm_embedding"].cpu().squeeze(),
+            }
+        
+        save_path = os.path.join(save_root, f"{filename}.pkl")
+        with open(save_path, "wb") as f:
+            pickle.dump(save_dict, f)
