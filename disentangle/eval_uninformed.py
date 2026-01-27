ad0bcc6c226fcc070e15f2450f4ae05b42ecf8f4
nmehlman <nmehlman@usc.edu>
2026-01-27 14:15:09 -0800

    Debug

--- disentangle/eval_uninformed.py
+++ disentangle/eval_uninformed.py
@@ -65,7 +65,7 @@ if __name__ == "__main__":
     
     # Load dataset and create dataloader
     dataset_class, dataset_sr = DATASETS[config["dataset"]]
-    dataset = dataset_class(**config["data_args"], split="val") # CHANGEME to test when ready
+    dataset = dataset_class(**config["data_args"], split="dev") # CHANGEME to test when ready
     
     # Process each sample
     for i, sample in tqdm.tqdm(enumerate(dataset), total=len(dataset), desc="Exporting Data"):
