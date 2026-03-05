269ba60e1bc6bb93d9f8801f8a66b7431d6a01f3
nmehlman <nmehlman@usc.edu>
2026-03-05 12:29:40 -0800

    Feats test

--- disentangle/misc/peft_ser_test.py
+++ disentangle/misc/peft_ser_test.py
@@ -7,5 +7,6 @@ import peft_ser
 model = peft_ser.load_model("whisper-base-lora-16-conv", cache_folder="/project2/shrikann_35/nmehlman/models/")
 
 data = torch.zeros([1, 2*16000])
-output = model(data, return_features=False)
+output, features = model(data, return_features=True)
 logits = torch.softmax(output, dim=1)
+print(features.shape)
