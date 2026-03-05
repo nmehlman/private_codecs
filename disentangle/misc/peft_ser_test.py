1654a810fdc03d4bd321012603bf93c95d6c80c8
Nick Mehlman <nmehlman@usc.edu>
2026-03-05 12:28:45 -0800

    peft test update

--- disentangle/misc/peft_ser_test.py
+++ disentangle/misc/peft_ser_test.py
@@ -6,6 +6,6 @@ import peft_ser
 
 model = peft_ser.load_model("whisper-base-lora-16-conv", cache_folder="/project2/shrikann_35/nmehlman/models/")
 
-data = torch.zeros([1, 16000])
-output = model(data, length=torch.tensor([16000]), return_features=False)
+data = torch.zeros([1, 2*16000])
+output = model(data, return_features=False)
 logits = torch.softmax(output, dim=1)
