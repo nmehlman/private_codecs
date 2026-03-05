202146239d691b59c1844ed3e23294de6685ed97
nmehlman <nmehlman@usc.edu>
2026-03-05 12:06:39 -0800

    Correct peft-ser path

--- disentangle/misc/peft_ser_test.py
+++ disentangle/misc/peft_ser_test.py
@@ -1,11 +1,11 @@
 import torch
 import sys
 
-sys.path.append("/Users/nick/Desktop/Private Codecs/peft-ser/package")
+sys.path.append("/home1/nmehlman/private_codecs/peft-ser/package")
 import peft_ser
 
 model = peft_ser.load_model("whisper-base-lora-16-conv", cache_folder="/project2/shrikann_35/nmehlman/models/")
 
-#data = torch.zeros([1, 16000])
-#output = model(data)
-#logits = torch.softmax(output, dim=1)
+data = torch.zeros([1, 16000])
+output = model(data)
+logits = torch.softmax(output, dim=1)
