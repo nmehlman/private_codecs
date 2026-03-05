bae46b3c0daf157f26a96b87d6e6efe2ab859e8d
nmehlman <nmehlman@usc.edu>
2026-03-05 12:16:50 -0800

    Add length to peft test call

--- disentangle/misc/peft_ser_test.py
+++ disentangle/misc/peft_ser_test.py
@@ -7,5 +7,5 @@ import peft_ser
 model = peft_ser.load_model("whisper-base-lora-16-conv", cache_folder="/project2/shrikann_35/nmehlman/models/")
 
 data = torch.zeros([1, 16000])
-output = model(data)
+output = model(data, length=torch.tensor([16000]), return_features=False)
 logits = torch.softmax(output, dim=1)
