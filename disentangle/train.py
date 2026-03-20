b05b34558852b2148c228d931a9697ecde407438
nmehlman <nmehlman@usc.edu>
2026-03-20 15:24:07 -0700

    Debuf

--- disentangle/train.py
+++ disentangle/train.py
@@ -106,6 +106,8 @@ class EpochInferenceCallback(Callback):
             audio_codec_only = torchaudio.functional.resample(
                 audio_codec_only, orig_freq=self.codec_sr, new_freq=self.dataset_sr
             ).unsqueeze(0)
+
+            print(audio_codec_only.shape, audio_private.shape)
             
             emotion_logits_private = self.emotion_model(
                     audio_private, sr=self.dataset_sr, return_embeddings=False, 
