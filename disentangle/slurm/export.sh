f1aa09a20c41d40405f31fcfc42340c98d9711c8
nmehlman <nmehlman@usc.edu>
2026-04-24 14:13:31 -0700

    Add slurm export script

--- disentangle/slurm/export.sh
+++ disentangle/slurm/export.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --nodes 1
 #SBATCH --partition gpu
-#SBATCH --time=5:00:00
+#SBATCH --time=3:10:00
 #SBATCH --account=shrikann_35
-#SBATCH --mem=20G
+#SBATCH --mem=40G
 #SBATCH --output=/project2/shrikann_35/nmehlman/logs/private_codecs/slurm/%j_output.log
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=2
 #SBATCH --gpus-per-node=a40:1
 #SBATCH --chdir /home1/nmehlman/private_codecs/private_codecs/disentangle
 #SBATCH --mail-type=ALL
@@ -15,6 +15,6 @@ module purge
 source /project2/shrikann_35/nmehlman/conda/etc/profile.d/conda.sh
 conda activate priv-codec
 
-CONFIG="configs/train/train_expresso_encodec_ae_only.yaml"
+CONFIG="$1"
 
-srun python train.py --config "$CONFIG" 
+srun python misc/export_data.py --config "$CONFIG" 
