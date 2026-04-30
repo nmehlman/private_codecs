d45750e57bd9ac2839b7d06cba1e0c86aa5a67c6
nmehlman <nmehlman@usc.edu>
2026-04-30 15:22:28 -0700

    Debug

--- network/foundation.py
+++ network/foundation.py
@@ -125,7 +125,7 @@ class WhisperWrapper(nn.Module):
 
         self.feature_extractor = AutoFeatureExtractor.from_pretrained(feat_id)
         self.backbone_model = WhisperModel.from_pretrained(model_id, output_hidden_states=True, ignore_mismatched_sizes=True,
-                max_source_positions=750)
+                max_source_positions=750).to(device)
         self.embed_positions = copy.deepcopy(self.backbone_model.encoder.embed_positions.weight)
         self.embed_positions.requires_grad = False
 
