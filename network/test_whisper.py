dbbc528bc2d8404f1af9cf4aed3848bedbd864ab
nmehlman <nmehlman@usc.edu>
2026-04-30 14:08:39 -0700

    Debug

--- network/test_whisper.py
+++ network/test_whisper.py
@@ -9,11 +9,11 @@ if __name__ == "__main__":
     model = WhisperWrapper(pretrain_model="whisper_large")
 
     print("With lengths:")
-    _, batch_emb, _, _, _, _ = model(x, length=length)
+    _, batch_emb, _, _, _, _ = model(x, length=length, return_feature=True)
 
     singleton_embs = []
     for i in range(x.shape[0]):
-        _, emb, _, _, _, _ = model(x[i:i+1], length=length[i:i+1])
+        _, emb, _, _, _, _ = model(x[i:i+1], length=length[i:i+1], return_feature=True)
         singleton_embs.append(emb)
 
     singleton_embs = torch.cat(singleton_embs, dim=0)
