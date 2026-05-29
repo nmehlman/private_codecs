6bc3721e9eec31368f13773310d1ba2988a85027
Nick Mehlman <nmehlman@usc.edu>
2026-05-29 03:24:12 -0700

    config

--- disentangle/lightning.py
+++ disentangle/lightning.py
@@ -193,7 +193,7 @@ class SexDisentangleModule(pl.LightningModule):
         
          # Zero AE grads, backprop fool_loss but keep graph for second backward
         opt_ae.zero_grad(set_to_none=True)
-        self.manual_backward(fool_loss retain_graph=True)
+        self.manual_backward(fool_loss, retain_graph=True)
         grads_adv = [p.grad.clone() if p.grad is not None else None for p in self.ae.parameters()]
 
         # Zero AE grads again, backprop recon_loss
