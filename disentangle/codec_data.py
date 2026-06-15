ec1f7615fead1350931b5dd83c359c61cca8d315
nmehlman <nmehlman@usc.edu>
2026-06-15 14:49:55 -0700

    Debug

--- disentangle/codec_data.py
+++ disentangle/codec_data.py
@@ -111,6 +111,7 @@ def get_dataloaders(
     if train_val_spk is not None:
         train_dset = EmbeddingDataset(**dataset_kwargs, split="train", speakers=train_val_spk['train'])
         val_dset = EmbeddingDataset(**dataset_kwargs, split="train", speakers=train_val_spk['val'])
+        breakpoint()
     else:
         full_dset = EmbeddingDataset(**dataset_kwargs, split="dev")
 
