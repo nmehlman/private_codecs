843df5b203e4cd01d9567079fcab8a3878c3c51a
nmehlman <nmehlman@usc.edu>
2026-06-17 10:36:57 -0700

    Implement simple Whisper interface

--- network/asr.py
+++ network/asr.py
@@ -1,6 +1,34 @@
 import torch
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 
+class WhisperASR(nn.Module):
+    def __init__(self, device="cpu", pretrain_model="openai/whisper-large-v3"):
+        
+        super().__init__()
+        self.device = device
+        self.pretrain_model = pretrain_model
+
+        torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+        self.model = AutoModelForSpeechSeq2Seq.from_pretrained(
+            self.pretrain_model, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+        ).to(self.device)
+
+        self.processor = AutoProcessor.from_pretrained(self.pretrain_model)
+        
+        self.pipe = pipeline(
+            "automatic-speech-recognition",
+            model=self.model,
+            tokenizer=self.processor.tokenizer,
+            feature_extractor=self.processor.feature_extractor,
+            torch_dtype=torch_dtype,
+            device=self.device,
+        )
+
+    def transcribe(self, x: torch.Tensor):
+        """Return per-sample transcriptions of shape (B,)."""
+        x = x.numpy()
+        results = self.pipe(x)
+        return [res["text"] for res in results]
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
