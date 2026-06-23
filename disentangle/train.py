c296e81bfa1a2b410963ffaeb40f283e998b8530
nmehlman <nmehlman@usc.edu>
2026-06-23 10:23:30 -0700

    Add JSON logging of final metrics

--- disentangle/train.py
+++ disentangle/train.py
@@ -300,3 +300,5 @@ if __name__ == "__main__":
     parsed_results = parse_results(results_dir) # Compute average metrics
     for key, value in parsed_results.items():
         print(f"{key}: {value:.4f}")
+
+    json.dump(parsed_results, open(os.path.join(results_dir, "final_results.json"), "w"), indent=4)
