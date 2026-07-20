99e38d6330a92bf48085523b6f5b44a7b4da4545
Nick Mehlman <nmehlman@usc.edu>
2026-07-20 07:39:28 -0700

    Config and slurm script

---
+++ disentangle/slurm/export_gender.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --nodes 1
+#SBATCH --partition gpu
+#SBATCH --time=40:00
+#SBATCH --account=shrikann_35
+#SBATCH --mem=40G
+#SBATCH --output=/project2/shrikann_35/nmehlman/logs/private_codecs/slurm/%j_output.log
+#SBATCH --cpus-per-task=2
+#SBATCH --gpus-per-node=a40:1
+#SBATCH --chdir /home1/nmehlman/private_codecs/private_codecs/disentangle
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=nmehlman@usc.edu
+
+module purge
+source /project2/shrikann_35/nmehlman/conda/etc/profile.d/conda.sh
+conda activate priv-codec
+
+
+git checkout contrastive-gender
+srun python misc/add_sex_embeddings.py --config configs/export/export_gender_vox1.yaml 
