0573a4378f8696d2185734bc6a4f6b4d7ba7e001
Nick Mehlman <nmehlman@usc.edu>
2026-07-30 12:20:46 -0700

    Add eval script

---
+++ disentangle/slurm/eval.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --nodes 1
+#SBATCH --partition gpu
+#SBATCH --time=00:50:00
+#SBATCH --account=shrikann_35
+#SBATCH --mem=20G
+#SBATCH --output=/project2/shrikann_35/nmehlman/logs/private_codecs/slurm/%j_output.log
+#SBATCH --cpus-per-task=4
+#SBATCH --gpus-per-node=a40:1
+#SBATCH --chdir /home1/nmehlman/private_codecs/private_codecs/disentangle/eval
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=nmehlman@usc.edu
+
+module purge
+source /project2/shrikann_35/nmehlman/conda/etc/profile.d/conda.sh
+conda activate priv-codec
+
+CONFIG="$1"
+
+srun python eval_uninformed.py --config configs/eval_uninformed_vox1_hificodec.yaml
